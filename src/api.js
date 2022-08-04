9032d631994cb25574d5d7451b49663f49b65333
Eric Rackauckas <erackauckas@gmail.com>
2022-08-04 14:26:39 -0700

    final submit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fe4ec40312msh0bfa4a9d48017cep1d07f6jsne71b80177c76',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY =  "aef48a32a8dd8eb16acff1fa54763457";
+
+export const WEATHER_FORECAST = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
